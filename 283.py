8a98b9d959b34db2a3d96c3feca9f021d019b944
Chun-Ming Lai <cmlaih@gmail.com>
2016-10-05 15:34:21 -0700

    Update 283.py

--- 283.py
+++ 283.py
@@ -13,3 +13,13 @@ class Solution(object):
         while(k<len(nums)):
             nums[k]=0
             k += 1
+
+"""
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+思路, 邊走邊把不是零的element移到前面, 然後記錄遇到幾個零, 最後等到跑完的時候, 再把記錄零的個數的值慢慢append到後面去
+就完成了
+
+"""
