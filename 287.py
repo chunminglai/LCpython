3bf8cd02069d159b13f1c940c0f65d68805919c4
Chun-Ming Lai <cmlaih@gmail.com>
2016-10-05 15:38:48 -0700

    Update 287.py

--- 287.py
+++ 287.py
@@ -19,3 +19,11 @@ class Solution(object):
                 low = mid + 1
         return low
                     
+"""
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), 
+prove that at least one duplicate number must exist. Assume that there is only one duplicate number, 
+find the duplicate one.
+
+思路：二元搜尋法, low 一開始等於 1, high = n-1, 先跑一次, 如果low~mid的次數大於mid的話, 那代表要找的那個數一定在low與mid之間
+不然就是再另一邊, 然後一直找到low = high為止, 那個數就是要找的target了
+"""
